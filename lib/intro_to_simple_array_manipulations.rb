96b680cc9c1995798e60fc04503989be056f1046
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-08-26 17:48:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -47,7 +47,7 @@ def using_delete (instructors, no_offense_steven)
   instructors.delete no_offense_steven
 end
 
-def using_delete_at (famous_robots,2)
+def using_delete_at (famous_robots)
  famous_robots.delete_at(2)
 end
   
